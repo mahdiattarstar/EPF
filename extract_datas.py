363d710af9e3e0e601f84c1bf0740980d71b9bdb
mahdi <mahdiattarzade34@gmail.com>
2024-12-12 23:08:45 +0330

    its committed

--- extract_datas.py
+++ extract_datas.py
@@ -1,46 +1,67 @@
 from bs4 import BeautifulSoup
 import requests
-import re
+from data_base import Data_base
 import sys
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
-#create a class for button and extracting data from a site
-class Extract_data(FloatLayout):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-#create a button for adding new data
-        self.button_add=Button(text='Add new data',
-                                size_hint=(0.5,0.1),
-                                pos_hint={'x':0.2,'y':0.1} ,
-                                  )
-        self.button_add.bind(on_press=self.request_site)
-        self.add_widget(self.button_add)
-    
-#request to the site
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+sys.stdout.reconfigure(encoding='utf-8')
+
+class Extract_data:
+    def __init__(self):
+            
+        self.add_new_data = Data_base()
+
     def request_site(self):
-      self.request=requests.get('https://bama.ir/car/pride-131')
-      self.request=self.request.text
-      self.soup=BeautifulSoup(self.request,'html.parser')
-      findall=self.soup.find_all('a',class_='bama-ad listing')
-      limited_findall=findall[:20]
-      for information in limited_findall:
-        name,year,operation,price=self.extract(information)
-    
-    def extract(self,information):
-       car=information.find('span',class_='text')
-       year=information.find('span',{'data-v-9d0e2d7a':''})
-       operation=information.find('span',class_='dir-ltr')
-       price=information.find('div',class_='bama-add__price-holder')
-       
-
-      
-      
-
-
-
-
-
-
-
-
+        session = requests.Session()
+        session.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+        })
+        try:
+            driver = webdriver.Chrome()
+            driver.get('https://bama.ir/car/pride-131')
         
+            for _ in range(5):
+                driver.execute_script('window.scrollBy(0, 1000);')
+
+            page_source=driver.page_source
+            driver.quit()
+        except requests.exceptions.HTTPError as e:
+            print('noo')
+            return
+        except requests.exceptions.RequestException as e:
+            print('its imposible')
+            return
+        self.findall(page_source)
+
+    def findall(self,page_source):
+
+        self.soup = BeautifulSoup(page_source, 'html.parser')
+        findall = self.soup.find_all('a', class_='bama-ad listing')
+
+#creating a list for sorting and collecting datas
+        lis_car=[]
+        for information in findall:
+            name, year, operation, price = self.extract(information)
+                                                                   
+            lis_car.append((name,year,operation,price))
+           
+        self.add_new_data.add_data_base(lis_car)
+        lis_car.clear()
+
+    def extract(self, information):
+#extracing just an specific value
+    
+        car = information.find('span', class_='text')
+        year = information.select_one('div.bama-ad__detail-row > span:first-child')
+        operation = information.find('span', class_='dir-ltr')
+        price = information.find('div', class_='bama-ad__price-holder')
+#getting their texts
+        text_car = car.get_text(strip=True) if car else 'Unknown'
+        text_year = year.get_text(strip=True) if year else 'Unknown'
+        text_operation = operation.get_text(strip=True) if operation else 'Unknown'
+        text_price = price.get_text(strip=True) if price else 'Unknown'
+
+        return text_car, text_year, text_operation, text_price
+      
+        
+    
